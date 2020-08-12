bdb2d5ecb39a5d28cbe5b2e366d570b1712f5794
Ramadhan Maulana <ramadhancm@gmail.com>
2020-08-12 19:44:46 +0700

    update Main Activity

--- app/src/main/java/com/example/githubuserfinder/ui/MainActivity.kt
+++ app/src/main/java/com/example/githubuserfinder/ui/MainActivity.kt
@@ -5,7 +5,6 @@ import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.githubuserfinder.R
-import com.example.githubuserfinder.util.hideKeyboard
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +13,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         etSearch.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH){
-                hideKeyboard()
                 Toast.makeText(this,"Start searching", Toast.LENGTH_SHORT).show()
                 return@setOnEditorActionListener true
             }

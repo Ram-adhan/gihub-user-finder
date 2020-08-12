0bdda23b6f26b6f757ac5455c198aa9bc5e4c997
Ramadhan Maulana <ramadhancm@gmail.com>
2020-08-12 19:55:16 +0700

    move ApiClient to DI Module

---
+++ app/src/main/java/com/example/githubuserfinder/di/NetworkModule.kt
@@ -0,0 +1,46 @@
+package com.example.githubuserfinder.di
+
+import com.example.githubuserfinder.BuildConfig
+import com.example.githubuserfinder.data.ApiService
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+class NetworkModule {
+    @Provides
+    @Singleton
+    fun provideOkhttp(): OkHttpClient {
+        val timeOut = 30L
+        return OkHttpClient().newBuilder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor.Level.BODY
+                } else {
+                    HttpLoggingInterceptor.Level.NONE
+                }
+            })
+            .readTimeout(timeOut, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService =
+        retrofit.create(ApiService::class.java)
+}

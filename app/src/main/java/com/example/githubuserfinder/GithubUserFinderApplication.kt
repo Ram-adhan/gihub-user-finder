fcc5ac300a0644765c5515eb9d7f9a9e2c3473d6
Ramadhan Maulana <ramadhancm@gmail.com>
2020-08-12 20:13:01 +0700

    create application class

--- app/src/main/java/com/example/githubuserfinder/GithubUserFinderApplication.kt
+++ app/src/main/java/com/example/githubuserfinder/GithubUserFinderApplication.kt
@@ -1,6 +1,14 @@
 package com.example.githubuserfinder
 
 import android.app.Application
+import com.example.githubuserfinder.di.AppComponent
+import com.example.githubuserfinder.di.DaggerAppComponent
 
 class GithubUserFinderApplication : Application() {
+    lateinit var component: AppComponent
+    override fun onCreate() {
+        super.onCreate()
+
+        component = DaggerAppComponent.builder().build()
+    }
 }

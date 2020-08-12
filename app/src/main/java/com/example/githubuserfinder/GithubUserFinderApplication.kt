8e4933f3f99b53169c89a274380b1357b9b86bb8
Ramadhan Maulana <ramadhancm@gmail.com>
2020-08-12 19:43:51 +0700

    update base application

--- app/src/main/java/com/example/githubuserfinder/GithubUserFinderApplication.kt
+++ app/src/main/java/com/example/githubuserfinder/GithubUserFinderApplication.kt
@@ -1,14 +1,6 @@
 package com.example.githubuserfinder
 
 import android.app.Application
-import com.example.githubuserfinder.di.ApplicationComponent
-import com.example.githubuserfinder.di.DaggerApplicationComponent
 
 class GithubUserFinderApplication : Application() {
-    lateinit var appComponent: ApplicationComponent
-
-    override fun onCreate() {
-        super.onCreate()
-        appComponent = DaggerApplicationComponent.create()
-    }
 }
